2c48649c5fb311b65e3b46768bec06d7fb4922cb
Bill Columbia <hello@billcolumbia.com>
2014-04-08 15:24:17 -0700

    init

---
+++ www/index.php
@@ -0,0 +1,20 @@
+<?php
+
+// Move plugins path to right above web root
+define('CRAFT_PLUGINS_PATH', realpath(dirname(__FILE__) . "/../plugins").'/');
+
+// Move templates path to right above web root
+define('CRAFT_TEMPLATES_PATH', realpath(dirname(__FILE__) . "/../templates").'/');
+
+// Path to your craft/ folder
+$craftPath = '../core';
+
+// Do not edit below this line
+$path = rtrim($craftPath, '/').'/app/index.php';
+
+if (!is_file($path))
+{
+	exit('Could not find your craft/ folder. Please ensure that <strong><code>$craftPath</code></strong> is set correctly in '.__FILE__);
+}
+
+require_once $path;
